75757a8468888e6ac83aa4cb82ea50debbe336d4
solomon141 <weymit2001@gmail.com>
2024-08-03 16:32:32 +0300

    vertical experience chronology

---
+++ app/Heart.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Heart() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-8 w-8 text-orange-500"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 015.656 5.656l-1.172 1.172L10 16.343l-5.656-5.657-1.172-1.172a4 4 0 010-5.656z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default Heart;
